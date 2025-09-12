6d0df60a161b509574f96b3ca08c847c3b6f0700
Eduardo Jerez <eduardojerez1410@gmail.com>
2025-09-12 17:09:20 -0300

    Refactor. apply Request Form

--- app/Http/Controllers/ReservationChangeStatusController.php
+++ app/Http/Controllers/ReservationChangeStatusController.php
@@ -1,31 +1,23 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ChangeReservationStatusRequest;
 use App\Models\Reservation;
-use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Response;
 
-class ReservationChangeStatusController extends Controller
+final class ReservationChangeStatusController extends Controller
 {
-    protected $posibleNextStatus = [
-        'PENDING',
-        'CONFIRMED',
-        'CANCELLED',
-        'CHECKED_IN'
-    ];
-
-    public function __invoke(Reservation $reservation, Request $request)
+    public function __invoke(Reservation $reservation, ChangeReservationStatusRequest $request): JsonResponse
     {
-        $validated = $request->validate([
-            'status' => 'required|in:' . implode(',', $this->posibleNextStatus)
-        ]);
-
-        $reservation->update(['status' => $validated['status']]);
+        $reservation->update(['status' => $request->getStatus()]);
 
         return response()->json([
             'data' => $reservation,
-            'message' => 'Reservation status change successfully'
+            'message' => 'Reservation status changed successfully'
         ], Response::HTTP_OK);
     }
 }
